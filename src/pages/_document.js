68d3f9e682127cc83649fe046b4790952fe55b9b
hardik-rl <hardik.radicalloop@gmail.com>
2025-09-11 23:12:38 +0530

    seo error fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,37 +1,64 @@
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//     return (
+//         <Html lang="en">
+//             <Head>
+//                 <meta name="robots" content="index, follow" />
+//                 <meta name="author" content="Blog App" />
+//                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+//                 <meta charSet="UTF-8" />
+
+//                 <meta
+//                     name="description"
+//                     content="Default description for Blog App. Read latest blogs and articles."
+//                 />
+//                 <meta
+//                     name="keywords"
+//                     content="blog, articles, next.js, homepage"
+//                 />
+
+//                 {/* <link rel="canonical" href="https://v0-new-project-sccvwp1p3kv-git-uichanges-hardik-rls-projects.vercel.app" /> */}
+
+//                 <link
+//                     rel="preload"
+//                     href="/fonts/Lato-Regular.woff"
+//                     as="font"
+//                     type="font/woff"
+//                     crossOrigin="anonymous"
+//                 />
+//             </Head>
+//             <body>
+//                 <Main />
+//                 <NextScript />
+//             </body>
+//         </Html>
+//     );
+// }
+
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <meta name="robots" content="index, follow" />
-                <meta name="author" content="Your Name or Company" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta charSet="UTF-8" />
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <meta
-                    name="description"
-                    content="Default description for Blog App. Read latest blogs and articles."
-                />
-                <meta
-                    name="keywords"
-                    content="blog, articles, next.js, homepage"
-                />
-
-                {/* <link rel="canonical" href="https://v0-new-project-sccvwp1p3kv-git-uichanges-hardik-rls-projects.vercel.app" /> */}
-
-                <link
-                    rel="preload"
-                    href="/fonts/Lato-Regular.woff"
-                    as="font"
-                    type="font/woff"
-                    crossOrigin="anonymous"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+        {/* Global font preload (fine to keep) */}
+        <link
+          rel="preload"
+          href="/fonts/Lato-Regular.woff"
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
