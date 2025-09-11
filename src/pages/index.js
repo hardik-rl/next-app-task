e1862f84acc959a7957896a4fcd23ba8e13933cf
hardik-rl <hardik.radicalloop@gmail.com>
2025-09-11 18:17:40 +0530

    removed canonical url from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
+       <Head>
         <title>Home | Blog App</title>
         <meta
           name="description"
           content="Welcome to the homepage of My Website. Explore our latest blog posts and updates."
         />
-        <link rel="canonical" href="https://v0-new-project-sccvwp1p3kv-git-uichanges-hardik-rls-projects.vercel.app" />
+        {/* <link rel="canonical" href="https://v0-new-project-sccvwp1p3kv-git-uichanges-hardik-rls-projects.vercel.app" /> */}
       </Head>
       <Blog />
     </>
